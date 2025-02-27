cb773e82342781d4425062422decddccda0da161
DouglasSimoesM <douglas.simoesm@gmail.com>
2025-02-27 18:20:36 -0300

    Initial commit

---
+++ src/main/java/tech/buildrun/btg/orderms/RabbitMqConfig.java
@@ -0,0 +1,9 @@
+package tech.buildrun.btg.orderms;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String ORDER_CREATED_QUEUE = "btg-order-created";
+}
